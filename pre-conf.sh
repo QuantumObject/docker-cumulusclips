cd8723ce7359314fe26af2a2b6097e5be77ff56a
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2018-09-24 08:47:48 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -12,16 +12,11 @@
  chown -R www-data:www-data /var/www/
 
 #required by cumulusclip site to be 777 but that is bad idea. 
- chmod -R 775 cc-core/logs
- chmod -R 775 cc-content/uploads/h264
- chmod -R 775 cc-content/uploads/webm
- chmod -R 775 cc-content/uploads/mobile
- chmod -R 775 cc-content/uploads/temp
- chmod -R 775 cc-content/uploads/thumbs
- chmod -R 775 cc-content/uploads/avatars
+ chmod -R 750 cc-core/logs
+ chmod -R 750 cc-content/uploads/{h264,webm,mobile,temp,thumbs,avatars}
  
- sed  -i 's/upload_max_filesize = 2M/upload_max_filesize = 350M/' /etc/php/7.2/apache2/php.ini
- sed  -i 's/post_max_size = 8M/post_max_size = 350M/' /etc/php/7.2/apache2/php.ini
+ sed  -i 's/upload_max_filesize = 2M/upload_max_filesize = 1550M/' /etc/php/7.2/apache2/php.ini
+ sed  -i 's/post_max_size = 8M/post_max_size = 1550M/' /etc/php/7.2/apache2/php.ini
  sed  -i 's/max_execution_time = 30/max_execution_time = 1500/' /etc/php/7.2/apache2/php.ini
  sed  -i 's/short_open_tag = Off/short_open_tag = On/' /etc/php/7.2/apache2/php.ini
  
